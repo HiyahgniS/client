473b9e67cbe839de617d7b74a6f6244daedd59e1
HiyahgniS <hiya088btit21@igdtuw.ac.in>
2024-01-28 23:57:00 +0530

    Commit

---
+++ src/constants/data.js
@@ -0,0 +1,7 @@
+export const categories = [
+    { id: 1, type: "Movies" },
+    { id: 2, type: "Music" },
+    { id: 3, type: "Sports" },
+    { id: 4, type: "Fashion" },
+    { id: 5, type: "Tech" }
+];
